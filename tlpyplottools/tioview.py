2e0731738713bdc6cb8890637c3ff8a4b7dcc998
Tom Kornack <kornack@twinleaf.com>
2021-10-18 13:59:02 -0400

    Simplify namespace, clean up.

--- tlpyplottools/tioview.py
+++ tlpyplottools/tioview.py
@@ -6,28 +6,15 @@ Author: Esme Rubinstein <rubinstein@twinleaf.com>, Tom Kornack <kornack@twinleaf
 """
 
 import matplotlib
-
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from matplotlib.figure import Figure
-
-import matplotlib.animation
-from matplotlib import style
-import matplotlib.pyplot
-
 import time
 import re
 import argparse
-
-import tkinter as tk
-from tkinter import ttk
-
+import tkinter
 import tlpyplot
 
 # styles and fonts
-matplotlib.use("TkAgg")
-LARGE_FONT = ("cronos pro", 25)
-NORM_FONT = ("cronos pro", 15)
-style.use("ggplot")
+# matplotlib.use("TkAgg")
+matplotlib.style.use("ggplot")
 
 def processCommandLineArgs():
     parser = argparse.ArgumentParser(prog='vectorMonitor', 
@@ -61,20 +48,20 @@ def findConnectedDevices(tio):
 
 # popup message general function
 def popupmsg(msg):
-    popup = tk.Tk()
+    popup = tkinter.Tk()
     popup.wm_title("!")
-    label = ttk.Label(popup, text = msg, font = NORM_FONT)
+    label = tkinter.ttk.Label(popup, text = msg)
     label.pack(side = 'top', fill = 'x', pady = 10)
-    B1 = ttk.Button(popup, text = "Ok", command = popup.destroy)
+    B1 = tkinter.ttk.Button(popup, text = "Ok", command = popup.destroy)
     B1.pack()
     popup.mainloop()
 
 # pause function
-def loadChart():
-    if rtp.pause == True:
-        rtp.pause = False
-    else:
-        rtp.pause = True
+# def loadChart():
+#     if rtp.pause == True:
+#         rtp.pause = False
+#     else:
+#         rtp.pause = True
 
 # set initial stream list by looking at connected devices
 def setDefaults(tio, vmrStreamList):
@@ -93,36 +80,36 @@ def createPlot(streamList, windowLength):
     plotter = tlpyplot.TLPyPlot(queueLength = windowLength, streamList = streamList)
     return plotter
 
-class graphInterface(tk.Tk):
+class graphInterface(tkinter.Tk):
     def __init__(self, tio, plotter, vmrStreamList, defaultStream, windowLength, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        #tk.Tk.iconbitmap(self, default="clienticon.ico")
-        tk.Tk.wm_title(self, "Twinleaf Monitor")
+        tkinter.Tk.__init__(self, *args, **kwargs)
+        #tkinter.Tk.iconbitmap(self, default="clienticon.ico")
+        tkinter.Tk.wm_title(self, "Twinleaf Monitor")
 
-        container = tk.Frame(self)
+        container = tkinter.Frame(self)
         container.pack(side="top", fill="both", expand = True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
-        menubar = tk.Menu(container)
-        filemenu = tk.Menu(menubar, tearoff=0)
+        menubar = tkinter.Menu(container)
+        filemenu = tkinter.Menu(menubar, tearoff=0)
         filemenu.add_command(label="Save", command = lambda: popupmsg("Not supported yet!"))
         filemenu.add_command(label="Quit", command=quit)
         menubar.add_cascade(label="File", menu=filemenu)
 
-        dataTF = tk.Menu(menubar, tearoff=1)
+        dataTF = tkinter.Menu(menubar, tearoff=1)
         #dataTF.add_command(label = "Pause/Resume", command = lambda: loadChart())
         #dataTF.add_command(label = "Change Window Length", command = lambda: changeTimeFrame())
         menubar.add_cascade(label = "Graph Settings", menu = dataTF)
 
-        helpmenu = tk.Menu(menubar, tearoff = 0)
+        helpmenu = tkinter.Menu(menubar, tearoff = 0)
         #helpmenu.add_command(label = "Tutorial", command = lambda: tutorial())
         menubar.add_cascade(label = "Help", menu = helpmenu)
 
-        tk.Tk.config(self, menu=menubar)
+        tkinter.Tk.config(self, menu=menubar)
 
         self.frames = {}
-        for F in (StartPage, Page1):
+        for F in (StartPage, GraphPage):
             frame = F(tio, plotter, vmrStreamList, defaultStream, windowLength, container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -132,31 +119,31 @@ class graphInterface(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
-class StartPage(tk.Frame):
+class StartPage(tkinter.Frame):
 
     def __init__(self, tio,plotter, vmrStreamList, defaultStream, windowLength, parent, controller):
-        tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Available Streams", font = LARGE_FONT, fg = 'dark green')
+        tkinter.Frame.__init__(self,parent)
+        label = tkinter.Label(self, text="Available Streams")
         label.pack()
 
-        description = tk.Label(self, text = "Load available streams by typing them into the entry box below.  Entered stream values should look like: 'vmr0.vector, vmr1.bar', where different streams are separated by a comma. Only one stream from each device can be added.", bg = 'white', wraplength = 500)
+        description = tkinter.Label(self, text = "Load available streams by typing them into the entry box below.  Entered stream values should look like: 'vmr0.vector, vmr1.bar', where different streams are separated by a comma. Only one stream from each device can be added.", bg = 'white', wraplength = 500)
         description.pack()
 
         def streamChart(tio):
-            subframe = tk.Frame(self)
+            subframe = tkinter.Frame(self)
             labels = []
             streamb = []
             deviceAttributes = findConnectedDevices(tio)
             i = 0
             n = 0           
             for device in deviceAttributes:
-                labels.append(tk.Label(subframe, text = device.upper(), font = NORM_FONT))
+                labels.append(tkinter.Label(subframe, text = device.upper()))
                 labels[i].grid(column = i, row = 1)
                 i+=1
                 if "vmr" in device.lower():
                     for j in range(len(vmrStreamList)):
                         stre = device.lower()+"." + vmrStreamList[j]
-                        streamb.append(tk.Label(subframe,text = stre))
+                        streamb.append(tkinter.Label(subframe,text = stre))
                         streamb[n].grid(column = i-1, row = j+2)
                         n +=1
             subframe.pack(pady = 20)
@@ -164,8 +151,8 @@ class StartPage(tk.Frame):
         streamChart(tio)
 
         def getStreamEntry():
-            subframe2 = tk.Frame(self)
-            e = ttk.Entry(subframe2)
+            subframe2 = tkinter.Frame(self)
+            e = tkinter.ttk.Entry(subframe2)
             e.insert(0, defaultStream)
             e.pack(side = 'left')
             e.focus_set()
@@ -194,33 +181,33 @@ class StartPage(tk.Frame):
                 else:
                     popupmsg("Only one stream from each device can be added.")  
 
-            b = ttk.Button(subframe2, text = "Load Streams", width = 10, command = callback)
+            b = tkinter.ttk.Button(subframe2, text = "Load Streams", width = 10, command = callback)
             b.pack(side = 'left')
             subframe2.pack()
 
         getStreamEntry()
 
-        button = ttk.Button(self, text="Go!",
-                            command=lambda: controller.show_frame(Page1))
+        button = tkinter.ttk.Button(self, text="Go!",
+                            command=lambda: controller.show_frame(GraphPage))
         button.pack()
-        quitbutton = ttk.Button(self, text = "Quit", command = quit)
+        quitbutton = tkinter.ttk.Button(self, text = "Quit", command = quit)
         quitbutton.pack()
 
-class Page1(tk.Frame):
+class GraphPage(tkinter.Frame):
     def __init__(self, tio, plotter, vmrStreamList, defaultStream, windowLength, parent, controller):
-        tk.Frame.__init__(self,parent)
+        tkinter.Frame.__init__(self,parent)
         
 
         def graphSettings():
-            subframe = tk.Frame(self)
-            subsubframe = tk.Frame(subframe)
-            label = tk.Label(subsubframe, text = "VMR Monitor", font = LARGE_FONT, fg = 'dark green')
-            label.grid(column = 0, row = 0, padx = 30)
+            subframe = tkinter.Frame(self)
+            subsubframe = tkinter.Frame(subframe)
+            # label = tkinter.Label(subsubframe, text = "TIO Monitor")
+            # label.grid(column = 0, row = 0, padx = 30)
 
-            label2 = ttk.Label(subsubframe, text = "Change Window Length (s): ", font = NORM_FONT)
+            label2 = tkinter.ttk.Label(subsubframe, text = "Window Duration (s)")
             label2.grid(column = 1, row = 0)
 
-            e = ttk.Entry(subsubframe)
+            e = tkinter.ttk.Entry(subsubframe)
             e.insert(0, windowLength)
             e.grid(column = 2, row = 0)
             e.focus_set()
@@ -231,19 +218,19 @@ class Page1(tk.Frame):
                 plotter.increaseQueueSize(windowLength)
                 print("set window length to", windowLength)
 
-            b = ttk.Button(subsubframe, text = "Submit", width = 5, command = callback)
-            b.grid(column = 3, row = 0)
+            b = tkinter.ttk.Button(subsubframe, text = "Submit", width = 5, command = callback)
+            b.grid(column = 3, row = 0, padx = 20)
 
-            subsubframe2 = tk.Frame(subframe)
+            subsubframe2 = tkinter.Frame(subframe)
 
-            pauseb = ttk.Button(subsubframe2, text = "Pause/Resume", command = lambda: loadChart())
-            pauseb.grid(column = 0, row = 0, padx = 20)
+            # pauseb = tkinter.ttk.Button(subsubframe2, text = "Pause/Resume", command = lambda: loadChart())
+            # pauseb.grid(column = 0, row = 0, padx = 20)
 
-            button1 = ttk.Button(subsubframe2, text="Back to Home",
+            button1 = tkinter.ttk.Button(subsubframe2, text="Back to Home",
                             command=lambda: controller.show_frame(StartPage))
-            button1.grid(column = 2, row = 0)
+            button1.grid(column = 2, row = 0, padx = 20)
 
-            quitbutton = ttk.Button(subsubframe2, text = "Quit", command = quit)
+            quitbutton = tkinter.ttk.Button(subsubframe2, text = "Quit", command = quit)
             quitbutton.grid(column = 3, row = 0, padx = 20)
 
             subsubframe.grid(row = 0, column = 0)
@@ -253,13 +240,13 @@ class Page1(tk.Frame):
 
         graphSettings()
 
-        canvas = FigureCanvasTkAgg(plotter.fig, self)
+        canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(plotter.fig, self)
         canvas.draw()
         canvas.get_tk_widget().pack(fill = "both", expand = True)
         
-        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar = matplotlib.backends.backend_tkagg.NavigationToolbar2Tk(canvas, self)
         toolbar.update()
-        canvas._tkcanvas.pack(side = tk.TOP, fill = "both" ,expand = True)
+        canvas._tkcanvas.pack(side = tkinter.TOP, fill = "both" ,expand = True)
 
 def main():
     # get DeviceSync
