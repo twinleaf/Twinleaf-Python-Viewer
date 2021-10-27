9cb5e6dd62be41c59ff5ceafa8bc1d06bf10ae6f
Esme Rubinstein <87450557+esmerubinstein@users.noreply.github.com>
2021-10-27 13:33:28 -0400

    Update tioview.py

--- tlpyplottools/tioview.py
+++ tlpyplottools/tioview.py
@@ -164,14 +164,14 @@ class StartPage(tkinter.Frame):
                 devList = []
                 for strstream in strlst:
                     strstream = strstream.split(".")
-                    print(strstream)
+                    print("Streams",strstream)
                     try:
                         devList.append(strstream[0])
                         dev = getattr(tio, strstream[0])
                         fullstream = getattr(dev, strstream[1])
                         newSList.append(fullstream)
                     except AttributeError:
-                        popupmsg("Not a valid stream.  Entered stream values should look like: 'vmr0.vector, vmr1.bar', where different streams are separated by a comma and a space.")
+                        popupmsg("Not a valid stream.  Entered stream values should look like: 'vmr0.vector, vmr1.bar', where different streams are separated by a comma.")
                         break
 
                 if len(devList) == len(set(devList)):
